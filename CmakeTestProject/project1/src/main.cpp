16b9d4dbe0f07cfc5ed1435737d870fad6822b15
Abner <asd3817401@gmail.com>
2025-04-23 21:39:11 +0800

    project1 main.cpp

--- CmakeTestProject/project1/src/main.cpp
+++ CmakeTestProject/project1/src/main.cpp
@@ -1,11 +1,18 @@
 #include "main.h"
+#include "condition_var_example.h"
+#include "future_async_example.h"
+
 int main(){
     std::cout << "-------------------------------------\n";
     std::cout << "this is project1 cmake example  \n\n";
     std::cout << "using api1::call(): " << api1::getins().call();
     std::cout << "using api2::call(): " << api2::getins().call(); 
+
+
+    // condition_var_example::getins().run();
+    // future_async_example::getins().run();
+    future_async_apply::getins().run();
     std::cout << "-------------------------------------\n";
-    printf("123\n");
 
     std::cout << "\nPress any key to exit!\n";
     std::getchar();
